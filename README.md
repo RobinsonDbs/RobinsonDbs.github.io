17e1d1cfd4c212732beab321200180f959e424a8
Robinson <dubois.robinson@hotmail.fr>
2019-01-03 20:45:27 +0100

    ajout du mode psyché

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mon site internet. 
 
-site better motherfucking website héberger sur mon github, avec une popup proposant d'aller sur le lien xxvideo. 
+Site better motherfucking website héberger sur mon github, avec une popup proposant d'aller sur le lien xxvideo. 
 
 Visiter le site ici : https://robinsondbs.github.io/
--- script.js
+++ script.js
@@ -7,6 +7,40 @@ function displayPopup(){
 	}
 }
 
-setTimeout(function(){
-	displayPopup();
-}, 10000);
+// setTimeout(function(){
+// 	displayPopup();
+// }, 10000);
+
+function changeStyle(){
+	//Je récupère tous les paragraphe de la page html
+	var paragraphe = document.querySelectorAll("p");
+	console.log(paragraphe)
+	//Je bouble sur tous les paragraphe
+	//La variable est décomposée en plein de p et j'assigne la couleur blue à chaque p
+	paragraphe.forEach(function(p){
+		console.log(p)
+		p.style.color = "blue";
+		p.style.fontFamily = "papyrus";
+	});
+
+	var title1 = document.querySelectorAll("h1");
+	title1.forEach(function(t){
+		t.style.color = "green";
+		t.style.fontFamily = "Comic Sans MS";
+	});
+
+	var title2 = document.querySelectorAll("h2");
+	title2.forEach(function(t){
+		t.style.color = "green";
+		t.style.fontFamily = "Comic Sans MS";
+	});
+
+	var title3 = document.querySelectorAll("h3");
+	title3.forEach(function(t){
+		t.style.color = "green";
+		t.style.fontFamily = "Comic Sans MS";
+	});
+
+}
+
+
